08c9be06424206b78fbca3b7f2b5219c85120962
Derrick Lin <drrckln@gmail.com>
2016-04-10 13:24:09 -0400

    completed chapter 4 exercises

---
+++ chap4exercises.hs
@@ -0,0 +1,22 @@
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = x == reverse x
+
+myAbs :: Integer -> Integer
+myAbs x = if x >= 0
+            then x
+            else (- x)
+
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f x y = ((second x, second y), (first x, first y))
+
+x = (+)
+
+f xs = w x 1
+    where w = length xs
+
+\x -> x
+
+\(x:xs) -> x
+
+f2 (a, b) = a
