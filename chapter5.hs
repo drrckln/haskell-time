4528c188db4a047284917e3e6c6b20860fcde1ef
Derrick Lin <drrckln@gmail.com>
2016-04-15 23:47:14 -0500

    parametric polymorphism intermission exercises

--- chapter5.hs
+++ chapter5.hs
@@ -3,3 +3,16 @@
 -- got to the explanation about Foldable t
 -- did two more intermission exercises
 -- read about polymorphism
+
+hyp1 :: a -> a -> a
+hyp1 x y = x
+
+hyp2 :: a -> a -> a
+hyp2 x y = y
+
+hyp3 :: a -> b -> b
+hyp3 x y = y
+-- I don't believe you can have any other implementations..
+-- you don't know what b is, so you have no functions/methods
+-- available. You can only return it.
+
