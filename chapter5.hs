af06e9b294d1124ea2f343a4e402c062eade1fdd
Derrick Lin <drrckln@gmail.com>
2016-04-17 23:54:34 -0700

    type signature guessing exercises

--- chapter5.hs
+++ chapter5.hs
@@ -17,3 +17,18 @@ hyp3 x y = y
 -- available. You can only return it.
 
 -- learned about polymorphic constants
+
+myConcat :: String -> String
+myConcat x = x ++ " yo"
+
+myMult :: Fractional a => a -> a
+myMult x = (x / 3) * 5
+
+myTake :: Int -> String
+myTake x = take x "hey you"
+
+myCom :: Int -> Bool
+myCom x = x > (length [1..10])
+
+myAlph :: Char -> Bool
+myAlph x = x < 'z'
