73f4e9fc951d4ae0734f93349406641ac1949f4a
Derrick Lin <drrckln@gmail.com>
2016-04-21 10:29:51 -0700

    got thru Ord

--- chapter6.hs
+++ chapter6.hs
@@ -1,3 +1,15 @@
 -- just starting
 -- quotRem gives the quotient and remainder in a tuple
 -- divMod does the same
+
+
+divideThenAdd :: Fractional a => a -> a -> a
+divideThenAdd x y = (x / y) + 1
+
+subtractThenAdd :: Num a => a -> a -> a
+subtractThenAdd x y = (x - y) + 1
+
+-- we don't have to write f :: (Num a, Fractional a) => a -> a -> a
+-- because it's redundant. Fractional is a subset of Num.
+-- More explicitly, any Fractional is guaranteed to implement Num.
+
