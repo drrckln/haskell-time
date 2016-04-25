e729b7635c8c4f4f2a2eda280285dfbd3f34a973
Derrick Lin <drrckln@gmail.com>
2016-04-25 09:36:55 -0700

    multiple choice and typechecking exercises

--- chapter6.hs
+++ chapter6.hs
@@ -15,3 +15,47 @@ subtractThenAdd x y = (x - y) + 1
 
 -- Enum and Show
 
+-- time for chapter exercises!
+
+{-
+data Person = Person Bool
+-- does not work because it's not deriving Show
+-- error should say "no instance of (Show (Bool -> Person))"
+
+printPerson :: Person -> IO ()
+printPerson person = putStrLn (show Person)
+-}
+{-
+data Mood = Blah | Woot deriving Show
+
+settleDown x = if x == Woot
+                  then Blah
+                  else x
+-- should not typecheck, since didn't derive Eq
+-- error should be "no instance of (Eq Mood)"
+-}
+
+
+data Mood = Blah | Woot deriving (Show, Eq)
+
+settleDown x = if x == Woot
+                  then Blah
+                  else x
+-- acceptable inputs are Blah and Woot
+-- settleDown 9 no instance for (Num Mood), 9 is Num a => a and it tries to
+-- match that with Mood. But Mood doesn't implement the Num typeclass.
+-- Blah > Woot should result in "no instance for (Ord Mood)"
+
+type Subject = String
+type Verb = String
+type Object = String
+
+data Sentence =
+    Sentence Subject Verb Object
+    deriving (Eq, Show)
+
+s1 = Sentence "dogs" "drool"
+s2 = Sentence "Julie" "loves" "dogs"
+-- should not, since s1 is incomplete? also what does "type" do
+-- OK, it does typecheck, you just can't use it really. It's partially
+-- applied. Makes sense, since Sentence is a value constructor.
