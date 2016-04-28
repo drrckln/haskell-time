0a7f806b706d0a17eab6826987b5a78cd3590594
Derrick Lin <drrckln@gmail.com>
2016-04-28 10:48:27 -0700

    chapter 7 intermission exercises

--- chapter7.hs
+++ chapter7.hs
@@ -2,3 +2,15 @@ myNum :: Integer
 myNum = 1
 
 myVal f = myNum
+
+-- 1. these are all the same
+-- 2. Num a => a -> a -> a
+-- 3.
+addOneIfOdd n = case odd n of
+    True -> f n
+    False -> n
+    where f = \n -> n + 1
+
+addFive = \x -> \y -> (if x > y then y else x) + 5
+
+mflip f x y= f y x
